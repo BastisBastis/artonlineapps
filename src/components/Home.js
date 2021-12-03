31129407818f46a94d2c33ebd25dc7ed1d254afe
BastisBastis <sebgus@gmail.com>
2021-12-03 09:28:38 +0100

    Added link to registration

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,7 @@ import SmartIcon from "../assets/images/smart.png"
 import TunerIcon from "../assets/images/tunericon.png"
 import PitchyBirdIcon from "../assets/images/pitchy-bird.png"
 import VellingeIcon from "../assets/images/VellingeKommun.png"
+import ArtistenLogo from "../assets/images/KSLogga.png"
 
 const Home = () => {
   
@@ -32,6 +33,8 @@ const Home = () => {
         <AppLink to="/metronome" title="Metronom" image={HomeIcon} />
         <AppLink to="/skissa" title="Skissa" image={HomeIcon} />
         <AppLink external noStretch to="https://www.vellinge.se/vellinge-kulturskola-artisten" title="Hemsida" image={VellingeIcon} />
+        
+        <AppLink external noStretch to="https://svvellinge.speedadmin.dk/registration#/" title="Anmälan" image={ArtistenLogo} />
       </div>
     </nav> 
     
