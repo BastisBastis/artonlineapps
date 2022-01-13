ad32b67fca52d2a1a8d5bb19a98d59b29742fed4
BastisBastis <sebgus@gmail.com>
2022-01-13 20:18:40 +0100

    stuff

--- src/components/Smart/Smart.js
+++ src/components/Smart/Smart.js
@@ -1,12 +1,64 @@
-import React from "react"
+import React, {useState} from "react"
 import SmartPlayer from "./smartplayer/src/components/SmartPlayer"
 import videoData from "./assets/Starwars"
 
+import styles from "./smartplayer/src/components/SmartPlayer.module.css"
+
 const Smart = ()=> {
+  const [video,setVideo] = useState(null)
   
-    return (<>
-            <SmartPlayer videoData={videoData} />
-        </>)
+  const instructionStyle={
+    padding:"0.5rem"
+  }
+  
+  const setResolution = (res)=>{
+    setVideo(videoData[res]);
+  }
+  
+  const buttonData=[
+    {title:"Stor fil",res:"high"},
+    {title:"Liten fil",res:"low"}
+  ];
+  
+  const buttons=buttonData.map(btn=>{
+    return (
+      <div 
+        className={styles.button}
+        onClick={()=>setResolution(btn.res)}
+        key={"selectVideo"+btn.res}
+        style={{
+          margin:"1rem",
+          textAlign:'center',
+          fontSize:"24px"
+        }}
+      >
+        {btn.title}
+      </div>
+    )
+  })
+  
+  if (video) {
+    return (
+            <SmartPlayer videoData={video} />
+        )
+  } else {
+   
+    return (
+      <>
+        <div style={instructionStyle}>
+          <i>För att starta filmen, klicka på "Ladda liten fil" eller "Ladda stor fil". Om du använder mobildata eller har långsam internetuppkoppling bör välja den lilla filen, annars den stora.</i>
+      <h2>Instruktioner:</h2>
+      <ul>
+        <li>Tryck på instrumentets namn för att ta bort instrumentet.</li>
+        <li>Tryck igen för att ta fram instrumentets bild och ljud igen.</li>
+        <li>Tryck på själva videon av ett instrument för att ta bort alla andra</li>
+      </ul>
+        </div>
+        {buttons}
+      </> 
+    )
+    
+  }
 }
 
 export default Smart;
--- src/components/Smart/assets/Starwars.js
+++ src/components/Smart/assets/Starwars.js
@@ -1,4 +1,5 @@
 import videoLow from "./starwars.mp4"
+import videoHigh from "./starwars2.mp4"
 
 import bas from "./audio/bas.mp3"
 import cel from './audio/cel.mp3'
@@ -13,7 +14,7 @@ import sla from "./audio/tru.mp3"
 import val from "./audio/vhn.mp3"
 import vio from "./audio/vln.mp3"
 
-const videoData = {
+const videoDataLow = {
   videoSrc:videoLow,
   videoRate:0.955,
   audioDelay:0.2,
@@ -37,5 +38,45 @@ const videoData = {
   startY:186,
   partHeight:179,
   frameMarginY:4,
+}
+
+/*
+width: 1920,
+                                height: 1080,
+                                partHeight: 268,
+                                audioFolder:folder,
+                                audioFiles:files,
+                                //startY:186, starwars.mp4
+                                startY: 276,
+*/
+const videoDataHigh={
+  videoSrc:videoHigh,
+  videoRate:0.955,
+  audioDelay:0.2,
+  duration:58.2,
+  audio:[
+    {title:"Trombon",src:trb},
+    {title:"Valthorn",src:val},
+    {title:"Klarinett",src:kla},
+    {title:"Klockspel",src:klo},
+    {title:"Trumpet",src:trp},
+    {title:"Saxofon",src:sax},
+    {title:"Elbas",src:bas},
+    {title:"Cello",src:cel},
+    {title:"Slagverk",src:sla},
+    {title:"Fiol",src:vio},
+    {title:"Tvärflöjt",src:flj},
+    {title:"Piano",src:pia}
+  ],
+  width:1920,
+  height:1080,
+  startY:276,
+  partHeight:268,
+  frameMarginY:4,
+}
+
+const videoData = {
+  low:videoDataLow,
+  high:videoDataHigh
 }
 export default videoData;
