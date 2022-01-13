76c594e25598fe50ff46e53e2afc0779182b3c81
BastisBastis <sebgus@gmail.com>
2022-01-13 20:10:03 +0100

    test

--- src/components/Smart/Smart.js
+++ src/components/Smart/Smart.js
@@ -1,8 +1,12 @@
 import React from "react"
-import SmartPlayer from "./smartplayer/src/components/SmartPlayer"
+//import SmartPlayer from "./smartplayer/src/components/SmartPlayer"
 import videoData from "./assets/Starwars"
 
 const Smart = ()=> {
+  
+  if (true)
+    return null
+  
     return (<>
             <SmartPlayer videoData={videoData} />
         </>)
