5aa5ca87ccb63ecebcd3b64c4681693d57dbf770
BastisBastis <sebgus@gmail.com>
2022-01-13 20:11:14 +0100

    gitignore

--- src/components/Smart/Smart.js
+++ src/components/Smart/Smart.js
@@ -1,11 +1,8 @@
 import React from "react"
-//import SmartPlayer from "./smartplayer/src/components/SmartPlayer"
+import SmartPlayer from "./smartplayer/src/components/SmartPlayer"
 import videoData from "./assets/Starwars"
 
 const Smart = ()=> {
-  
-  if (true)
-    return null
   
     return (<>
             <SmartPlayer videoData={videoData} />
